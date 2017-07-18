db1cdd302336ec875b9672605910f8b57de2e617
Tristan Kerbo <Tristan.Kerbo@Qorvo.com>
2017-07-18 11:49:37 -0700

    added main method that prints hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,12 @@
 package edu.pcc.cis.helloworld;
 
+/**
+ * Created by Tristan Kerbo on 07/18/2017
+ */
+
 public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+    }
 }
