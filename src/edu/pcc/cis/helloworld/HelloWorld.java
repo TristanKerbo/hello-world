b08502522764f593eea69c174c5dd9d32ad570ef
Tristan Kerbo <Tristan.Kerbo@Qorvo.com>
2017-07-18 11:51:33 -0700

    print date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,5 +1,6 @@
 package edu.pcc.cis.helloworld;
 
+import java.util.Date;
 /**
  * Created by Tristan Kerbo on 07/18/2017
  */
@@ -8,5 +9,6 @@ public class HelloWorld {
     public static void main(String[] args)
     {
         System.out.println("Hello, World!");
+        System.out.println(new Date());
     }
 }
